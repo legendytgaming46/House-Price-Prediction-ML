56379b627b73263b1b46bfb5dd425a914897a341
Aditya Gupta <legendytgaming46@gmail.com>
2025-12-28 16:35:19 +0530

    Refactor model training and update caching method
    Refactor model training into a separate function and update cache decorator.

--- app.py
+++ app.py
@@ -29,8 +29,12 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
 
-model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+@st.cache_data
+def model_run()
+ model = RandomForestRegressor()
+ model.fit(X,y)
+ return model
+model = model_run()
 house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting House Price'):
@@ -38,6 +42,7 @@ with st.spinner('Predicting House Price'):
 msg = f'''House Price is:${round(house_price*100000,2)}'''
 st.success(msg)
 st.markdown('''**Desing and Devlovped by:Adiya Gupta**''')
+
 
 
 
