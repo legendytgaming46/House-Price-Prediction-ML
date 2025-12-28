45b3c8d6c32791a897c9300691f34b5359895a14
Aditya Gupta <legendytgaming46@gmail.com>
2025-12-28 15:14:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,20 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+
 st.title('🏠House Price prediction using ML')
+st.sidebar.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif')
+
+df = pd.read_csv('house_data.csv')
+X = df.iloc[:,:-3]
+y = df.iloc[:,-1]
+
+st.sidebar.title('Select House Features')
 st.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif')
+
+all_value = []
+for i in X:
+ ans = st.sidebar.slider(f'Select {i} value')
+ all_value.append(ans)
+
+st.write(all_value)
