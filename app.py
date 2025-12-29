cd701d1a60efe41dc7b89efedac407e6c6c9d64a
Aditya Gupta <legendytgaming46@gmail.com>
2025-12-29 14:02:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import time
 from sklearn.datasets import fetch_california_housing
 from sklearn.ensemble import RandomForestRegressor
 st.title('🏠 House Price prediction using ML')
-st.image('https://i.pinimg.com/originals/bd/a3/ed/bda3edbcdde12a14c477648671e6b9d2.gif')
-
+st.image('https://i.pinimg.com/originals/d3/18/13/d3181322e4522cf897fa8c1a038c6a2d.gif')
 df = pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
 st.sidebar.title('🏠 Select House features')
-st.sidebar.image('https://i.pinimg.com/originals/d3/18/13/d3181322e4522cf897fa8c1a038c6a2d.gif')
+st.sidebar.image('https://i.pinimg.com/originals/bd/a3/ed/bda3edbcdde12a14c477648671e6b9d2.gif')
 all_value = []
 for i in X:
   min_value = int(X[i].min())
@@ -42,5 +41,6 @@ msg = f'''House price is: $ {round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Aditya Gupta**''')
+
 
 
